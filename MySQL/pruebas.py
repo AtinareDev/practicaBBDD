2a68ce0ba0a70882705a8100257914c79ab6d2c7
Atinare <atinare.dev@gmail.com>
2025-03-22 14:27:44 +0100

    .

--- MySQL/pruebas.py
+++ MySQL/pruebas.py
@@ -28,13 +28,31 @@ BD_MySql.insertar_varios_datos(cursor, [
 # Confirmar los cambios en la base de datos
 conexion.commit()
 
-# Consultar los datos de un usuario
-cursor.execute("SELECT * FROM usuarios WHERE nombre = 'Pedro'")
+cursor.execute("SELECT * FROM usuarios")
 
-# Obtener todos los resultados
-ana = cursor.fetchall()
+# Usando fetchall()
+todos_los_resultados = cursor.fetchall()
+print("Todos los resultados:", todos_los_resultados)
 
-# Imprimir los resultados
-print(ana)
+cursor.execute("SELECT * FROM usuarios where edad > 30")
+# Usando fetchall()
+# Consumo de los datos de la consulta anterior al asignar a la variable
+todos_los_resultados = cursor.fetchall()
+print("Todos los resultados de + de 30:", todos_los_resultados)
+
+# Como el cursor al hacer fetchall ha llegado al final de los resultados, 
+# no se puede volver a usar fetchall() para obtener los mismos resultados.
+# Por lo tanto, se vuelve a ejecutar la consulta para obtener los resultados 
+# nuevamente. Es importante recordar que hay que consumir los resultados del cursor
+# antes de volver a ejecutar la consulta o dara error Unread result found.
+cursor.execute("SELECT * FROM usuarios where edad > 30")
+
+# Usando fetchone() para obtener el primer resultado
+primer_resultado = cursor.fetchone()
+print("Primer resultado:", primer_resultado)
+
+# Usando fetchmany() para obtener las primeras 2 filas
+primeras_dos_filas = cursor.fetchmany(2)
+print("Primeras dos filas:", primeras_dos_filas)
 
 
